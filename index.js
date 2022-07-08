7be19b3b709e6758acf3298d1ee79e7d4026ce4a
Ashish <ashishsark98@gmail.com>
2022-07-09 00:06:09 +0530

    Complete functioning of tip calculator(desktop)

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function isNumberKey(evt)
         if (charCode!=8 && charCode != 46 && charCode > 31 && (charCode < 48 || charCode > 57))
             return false; 
         else{
-        if(evt.srcElement.className!="form-control bill"){
+        if(evt.srcElement.className=="form-control people"){
             if (evt.charCode==48 && b==false && evt.srcElement.value==0)
             {
                 $("<span class='warn'>Can't be zero</span>").insertBefore(".people");
@@ -21,7 +21,45 @@ function isNumberKey(evt)
         }
     }
 
-$(".people").on("click", function() {
-    if ($(this).val() != "")
-        $(this).val("");
+$(".reset").on("click",function(){
+    tip=$(".btn.selected").val();
+    bill=$(".bill").val();
+    numOfPeople=$(".people").val();
+    calculate(tip,bill,numOfPeople);
 });  
+
+$(".btn").on("click",select);
+$("input.btn").on("click",function(){
+    $(this).addClass("inputselect");
+});
+function select(){
+    $(".btn").removeClass("inputselect");
+    $(".btn").removeClass("selected");
+    $(this).addClass("selected");
+}
+
+$( "form" ).submit(function( event ) {
+    event.preventDefault();
+  });
+
+function calculate(t,b,n){
+    if(b!="" && n!="" && t!="" && Number.isInteger(Number(n))){
+    b=Number(b);
+    n=Number(n).toFixed(0);
+    t=Number(t);
+    var ta= (b*t)/(n*100);
+    var tot= (b/n)+ta;
+    $(".tipamt").text("$"+ta.toFixed(2));
+    $(".total").text("$"+tot.toFixed(2));
+    $(".people").text(n);
+    }
+    else if(Number.isInteger(Number(n))==false){
+        alert("Number of People should be Integer");
+    }
+    else{
+        alert("Enter required data");
+    }
+}
+
+
+
