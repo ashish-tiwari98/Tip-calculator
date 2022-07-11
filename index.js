8a73ad6a6cd835ead9938f0623bcf5ff4a4b7bfe
Ashish <ashishsark98@gmail.com>
2022-07-11 22:48:41 +0530

    Completed without form design

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-
-
-// $(".reset").on("click",function(){
-//     tip=$(".btn.selected").val();
-//     bill=$(".bill").val();
-//     numOfPeople=$(".people").val();
-//     calculate(tip,bill,numOfPeople);
-// });  
+let prevVal = "";
+    document.querySelector('input').addEventListener('input', function(e){
+      if(this.checkValidity()){
+        prevVal = this.value;
+      } else {
+        this.value = prevVal;
+      }
+    });
 
 $(".btn").on("click",select);
 $("input.btn").on("click",function(){
@@ -17,23 +17,35 @@ function select(){
     $(this).addClass("selected");
 }
 
+$(".bill, .people, .btn").change(validate);
+$(".btn").click(validate)
+function validate(){
+  let tip=($(".btn.selected").val());
+  let bill=($(".bill").val());
+  let numOfPeople=($(".people").val());
+  if(bill!="" && numOfPeople!="" && tip!="")
+    calculate(tip,bill,numOfPeople);
+}
 
+function calculate(t,b,n){
+    if(Number.isInteger(Number(n))){
+      b=Number(b);
+      n=Number(n).toFixed(0);
+      t=Number(t);
+      var ta= (b*t)/(n*100);
+      var tot= (b/n)+ta;
+      $(".tipamt").text("$"+ta.toFixed(2));
+      $(".total").text("$"+tot.toFixed(2));
+      $(".people").text(n);
+    }
+    else if(Number.isInteger(Number(n))==false){
+      alert("Number of People should be Integer");
+    }
+    else{
+      alert("Enter required data");
+    }
+}
 
-// function calculate(t,b,n){
-//     if(b!="" && n!="" && t!="" && Number.isInteger(Number(n))){
-//     b=Number(b);
-//     n=Number(n).toFixed(0);
-//     t=Number(t);
-//     var ta= (b*t)/(n*100);
-//     var tot= (b/n)+ta;
-//     $(".tipamt").text("$"+ta.toFixed(2));
-//     $(".total").text("$"+tot.toFixed(2));
-//     $(".people").text(n);
-//     }
-//     else if(Number.isInteger(Number(n))==false){
-//         alert("Number of People should be Integer");
-//     }
-//     else{
-//         alert("Enter required data");
-//     }
-// }
+$(".reset").on("click",function(){
+  $(".bill, .btn.selected, .people").val("");
+});
